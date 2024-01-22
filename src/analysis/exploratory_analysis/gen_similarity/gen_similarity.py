d5bccb85d7b2f54785a85642b54efd8b11da3351
Nick McIntosh <nicholas.w.mcintosh@gmail.com>
2024-01-22 00:40:40 -0500

    Added word count graph from Excel and added while loop to gen_similarity.py for easy multiple queries

--- src/analysis/exploratory_analysis/gen_similarity/gen_similarity.py
+++ src/analysis/exploratory_analysis/gen_similarity/gen_similarity.py
@@ -4,6 +4,16 @@ from gensim.models.keyedvectors import KeyedVectors
 def gen_similarity(model_name):
     model = KeyedVectors.load_word2vec_format(
         f'outputs/model_outputs/txt_models/{model_name}.txt', binary=False)
-    word = input('What word would you like to query? ')
-    results = model.most_similar(positive=[word])
-    print(results)
+
+    while True:
+        word = input('What word would you like to query? ')
+        results = model.most_similar(positive=[word])
+        for index, result in enumerate(results, start=1):
+            print(f"{index}. {result}")
+        another_query = input(
+            "Would you like to query another word? (y/n): ").strip().lower()
+        if another_query != 'y':
+            break
+
+
+gen_similarity('top_words_model_1.0')
